071bce89e5f57c1c238f1bdfdac87f25c4fd5fb0
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-04-08 01:56:56 +0200

    Server deploy

---
+++ heartbleed.filippo.io.py
@@ -0,0 +1,26 @@
+import web
+import subprocess as sp
+import json
+
+urls = (
+    '/bleed/(.*)', 'bleed',
+    '/.*', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self):
+        raise web.found('http://filippo.io/Heartbleed')
+
+class bleed:
+    def GET(self, host):
+        if not ':' in host: host += ':443'
+
+        child = sp.Popen(['heartbleed', host], stdout=sp.PIPE)
+        data = child.communicate()[0]
+        rc = child.returncode
+
+        return json.dumps({'code': rc, 'data': data})
+
+if __name__ == "__main__":
+    app.run()
