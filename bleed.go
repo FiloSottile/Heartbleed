7be9ad63168eab20ca60e2ce467bfd550526a72d
Kyle Isom <kyle@cloudflare.com>
2014-04-07 22:02:37 -0700

    improvements to bleed.go

--- bleed.go
+++ bleed.go
@@ -10,9 +10,12 @@ import (
 
 func main() {
 	out, err := bleed.Heartbleed(os.Args[1], []byte("YELLOW SUBMARINE"))
-	if err != nil {
+	if err == bleed.ErrPayloadNotFound {
 		log.Printf("%v - SAFE", os.Args[1])
 		os.Exit(1)
+	} else if err != nil {
+		log.Printf("%v - ERROR: %v", os.Args[1], err)
+		os.Exit(2)
 	} else {
 		log.Printf("%v\n", string(out))
 		log.Printf("%v - VULNERABLE", os.Args[1])
