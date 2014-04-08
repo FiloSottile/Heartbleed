6d5cfbca697f7885bebdf49ed3634e4b0a2f1654
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-04-08 06:55:15 +0200

    add web server

--- bleed.go
+++ bleed.go
@@ -1,3 +1,5 @@
+//+build skip
+
 package main
 
 import (
---
+++ bleed_serve.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	bleed "github.com/FiloSottile/Heartbleed/bleed"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var PAYLOAD = []byte("heartbleed.filippo.io")
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "http://filippo.io/Heartbleed", http.StatusFound)
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hi there.")
+}
+
+type result struct {
+	Code int    `json:"code"`
+	Data []byte `json:"data"`
+}
+
+func bleedHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	host := r.URL.Path[len("/bleed/"):]
+	if strings.Index(host, ":") == -1 {
+		host = host + ":443"
+	}
+	data, err := bleed.Heartbleed(string(host), PAYLOAD)
+	var rc int
+	if err == bleed.ErrPayloadNotFound {
+		rc = 1
+		data = []byte("")
+		log.Printf("%v - SAFE", host)
+	} else if err != nil {
+		rc = 2
+		data = []byte("")
+		log.Printf("%v - ERROR", host)
+	} else {
+		rc = 0
+		log.Printf("%v - VULNERABLE", host)
+	}
+	res := result{rc, data}
+	j, err := json.Marshal(res)
+	if err != nil {
+		log.Println("ERROR", err)
+	}
+	w.Write(j)
+}
+
+func main() {
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/bleed/", bleedHandler)
+	http.ListenAndServe(":8080", nil)
+}
