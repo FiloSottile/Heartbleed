9d25020364a60b8557dc65c89bec9263fe14d38d
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-04-27 14:07:31 +0200

    Merge bleed_serve_moz.go and bleed_serve.go

--- server/README.md
+++ server/README.md
@@ -1,3 +1,19 @@
 The Heartbleed test server. With caching by Mozilla.
 
 Install the `SAMPLE.aws-config.json` as `~/.aws-config.json` or `/etc/aws-config.json` or in a path specified by the `GODYNAMO_CONF_FILE` env var.
+
+```
+Usage:
+  HBserver --redir-host=<host> [--listen=<addr:port> --expiry=<duration>]
+  HBserver -h | --help
+  HBserver --version
+
+Options:
+  --redir-host HOST   Redirect requests to "/" to this host.
+  --listen ADDR:PORT  Listen and serve requests to this address:port [default: :8082].
+  --expiry DURATION   ENABLE CACHING. Expire records after this period.
+                      Uses Go's parse syntax
+                      e.g. 10m = 10 minutes, 600s = 600 seconds, 1d = 1 day, etc.
+  -h --help           Show this screen.
+  --version           Show version.
+```
--- server/bleed_serve.go
+++ server/bleed_serve.go
@@ -1,26 +1,20 @@
-//+build skip
-
 package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 
+	"github.com/docopt/docopt-go"
+
 	bleed "github.com/FiloSottile/Heartbleed/bleed"
+	cache "github.com/FiloSottile/Heartbleed/server/cache"
 )
 
-var PAYLOAD = []byte("heartbleed.filippo.io")
+var PAYLOAD = []byte("filippo.io/Heartbleed")
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://filippo.io/Heartbleed", http.StatusFound)
-}
-
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hi there.")
-}
+var withCache bool
 
 type result struct {
 	Code  int    `json:"code"`
@@ -32,57 +26,73 @@ type result struct {
 func handleRequest(tgt *bleed.Target, w http.ResponseWriter, r *http.Request, skip bool) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	data, err := bleed.Heartbleed(tgt, PAYLOAD, skip)
 	var rc int
 	var errS string
+	var data string
 
-	if err == bleed.Safe || err == bleed.Closed {
-		rc = 1
-	} else if err != nil {
-		rc = 2
-	} else {
-		rc = 0
-		// _, err := bleed.Heartbleed(tgt, PAYLOAD)
-		// if err == nil {
-		// 	// Two VULN in a row
-		// 	rc = 0
-		// } else {
-		// 	// One VULN and one not
-		// 	_, err := bleed.Heartbleed(tgt, PAYLOAD)
-		// 	if err == nil {
-		// 		// 2 VULN on 3 tries
-		// 		rc = 0
-		// 	} else {
-		// 		// 1 VULN on 3 tries
-		// 		if err == bleed.Safe {
-		// 			rc = 1
-		// 		} else {
-		// 			rc = 2
-		// 		}
-		// 	}
-		// }
+	var cacheOk bool
+	if withCache {
+		cReply, ok := cache.Check(tgt.HostIp)
+		if ok {
+			rc = int(cReply.Status)
+			cacheOk = true
+		}
 	}
 
-	switch rc {
-	case 0:
-		log.Printf("%v (%v) - VULNERABLE [skip: %v]", tgt.HostIp, tgt.Service, skip)
-	case 1:
-		data = ""
-		log.Printf("%v (%v) - SAFE", tgt.HostIp, tgt.Service)
-	case 2:
-		data = ""
-		errS = err.Error()
-		if errS == "Please try again" {
-			log.Printf("%v (%v) - MISMATCH", tgt.HostIp, tgt.Service)
+	if !withCache || !cacheOk {
+		out, err := bleed.Heartbleed(tgt, PAYLOAD, skip)
+
+		if err == bleed.Safe || err == bleed.Closed {
+			rc = 1
+		} else if err != nil {
+			rc = 2
 		} else {
-			log.Printf("%v (%v) - ERROR [%v]", tgt.HostIp, tgt.Service, errS)
+			rc = 0
+			// _, err := bleed.Heartbleed(tgt, PAYLOAD)
+			// if err == nil {
+			// 	// Two VULN in a row
+			// 	rc = 0
+			// } else {
+			// 	// One VULN and one not
+			// 	_, err := bleed.Heartbleed(tgt, PAYLOAD)
+			// 	if err == nil {
+			// 		// 2 VULN on 3 tries
+			// 		rc = 0
+			// 	} else {
+			// 		// 1 VULN on 3 tries
+			// 		if err == bleed.Safe {
+			// 			rc = 1
+			// 		} else {
+			// 			rc = 2
+			// 		}
+			// 	}
+			// }
+		}
+
+		if withCache {
+			cache.Set(tgt.HostIp, rc)
+		}
+
+		switch rc {
+		case 0:
+			data = out
+			log.Printf("%v (%v) - VULNERABLE [skip: %v]", tgt.HostIp, tgt.Service, skip)
+		case 1:
+			log.Printf("%v (%v) - SAFE", tgt.HostIp, tgt.Service)
+		case 2:
+			errS = err.Error()
+			if errS == "Please try again" {
+				log.Printf("%v (%v) - MISMATCH", tgt.HostIp, tgt.Service)
+			} else {
+				log.Printf("%v (%v) - ERROR [%v]", tgt.HostIp, tgt.Service, errS)
+			}
 		}
 	}
 
 	res := result{rc, data, errS, tgt.HostIp}
 	j, err := json.Marshal(res)
 	if err != nil {
-		log.Println("ERROR", err)
+		log.Println("[json] ERROR:", err)
 	} else {
 		w.Write(j)
 	}
@@ -126,12 +136,47 @@ func bleedQueryHandler(w http.ResponseWriter, r *http.Request) {
 	handleRequest(&tgt, w, r, s)
 }
 
+var Usage = `Heartbleed test server.
+
+Usage:
+  HBserver --redir-host=<host> [--listen=<addr:port> --expiry=<duration>]
+  HBserver -h | --help
+  HBserver --version
+
+Options:
+  --redir-host HOST   Redirect requests to "/" to this host.
+  --listen ADDR:PORT  Listen and serve requests to this address:port [default: :8082].
+  --expiry DURATION   ENABLE CACHING. Expire records after this period.
+                      Uses Go's parse syntax
+                      e.g. 10m = 10 minutes, 600s = 600 seconds, 1d = 1 day, etc.
+  -h --help           Show this screen.
+  --version           Show version.`
+
 func main() {
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/test", testHandler)
+	arguments, _ := docopt.Parse(Usage, nil, true, "HBserver 0.3", false)
+
+	if arguments["--expiry"] != nil {
+		withCache = true
+	}
+
+	if withCache {
+		cache.Init(arguments["--expiry"].(string))
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, arguments["--redir-host"].(string), http.StatusFound)
+	})
+
+	// Required for some ELBs
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
 	http.HandleFunc("/bleed/", bleedHandler)
 	http.HandleFunc("/bleed/query", bleedQueryHandler)
-	err := http.ListenAndServe(":80", nil)
+
+	log.Printf("Starting server on %s\n", arguments["--listen"].(string))
+	err := http.ListenAndServe(arguments["--listen"].(string), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
