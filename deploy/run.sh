80e5760d42215ecd55a74103ea2823e821fe7b10
Filippo Valsorda <filippo.valsorda@gmail.com>
2014-10-31 13:49:00 +0000

    Make the cache last 6h

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,5 +2,5 @@
 
 cd /home/ubuntu
 
-exec sudo ./HBserver --redir-host="https://filippo.io/Heartbleed" --listen=":80" --expiry="1h" 2>&1 | tee -a ./heartbleed.log
+exec sudo ./HBserver --redir-host="https://filippo.io/Heartbleed" --listen=":80" --expiry="6h" 2>&1 | tee -a ./heartbleed.log
 
